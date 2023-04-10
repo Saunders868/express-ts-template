3a12d0884ce64d558a3ea7588bff2ecd3a94b140
Daniel Saunders <saundersdaniel.10@gmail.com>
2023-04-10 16:13:45 -0400

    added code to include client url in cors allowed origins

--- src/utils/server.ts
+++ src/utils/server.ts
@@ -8,7 +8,7 @@ function createServer(): Express {
   app.use(
     cors({
       credentials: true,
-      origin: "http://localhost:5173",
+      origin: ["http://localhost:5173", process.env.CLIENT_URL!],
     })
   );
 
